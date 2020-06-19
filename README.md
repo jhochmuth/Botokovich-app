570e56723bcdeea7aee3118bf9644fa416bd36b0
jhochmuth <jhochmuth@comcast.net>
2020-06-18 21:18:08 -0600

    Client has embedded Flat editor.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Botokovich (web app version)
+
+This app was bootstrapped using [fastapi-react-demo](https://github.com/rcdilorenzo/fastapi-react-demo).
--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
+import Embed from 'flat-embed';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = { verse: 'Loading...' };
+    super(props);
+    this.embed = null;
   }
 
   componentDidMount() {
+    /*
     fetch('/api/verse/Genesis/1/1')
       .then(r => r.json())
       .then(data => this.setState({ text: data.text }));
+    */
+    const container = document.getElementById('flat-container');
+
+    this.embed = new Embed(container, {
+      score: '5eec27dee4645c2df0df0857',
+      embedParams: {
+        mode: 'edit',
+        appId: '5ee199bd36e3a0440978997a',
+        controlsPosition: 'top'
+      }
+    });
+  }
+
+  createMidi() {
+
   }
 
   render() {
     return (
-      <div>
-        <h1>fastapi-react-demo</h1>
-        <p>Result of API call: {this.state.text}</p>
+      <div className="App">
+        <div id="flat-container" />
+        <button onClick={() => this.createMidi()}>Create midi</button>
       </div>
     );
   }
